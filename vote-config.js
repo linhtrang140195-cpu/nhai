3618e7f85228f526ba6836094cacadf5d896aba7
Tran Thi Linh Trang [VEE0500] <linhtrang.tran@garena.vn>
2026-07-10 13:06:05 +0700

    add

---
+++ vote-config.js
@@ -0,0 +1,10 @@
+window.NHAI_VOTE_CONFIG = {
+  enabled: true,
+  supabaseUrl: "https://xmtxdfeengpbapgudprx.supabase.co",
+  anonKey: "sb_publishable_0uqGPTS2u3F_afGtgn8DRQ_yjxqjdHR",
+  edgeFunctionUrl: "",
+  analyticsEnabled: true,
+  analyticsFunctionUrl: "",
+  campaignId: "top-pick-day01-2026",
+  maxVotes: 3
+};
